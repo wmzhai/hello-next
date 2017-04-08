cea8973b949d228008307ba9b7694c6739c66c37
Weiming Zhai <wmzhai@gmail.com>
2017-04-08 10:42:36 +0800

    use button as link

--- pages/index.js
+++ pages/index.js
@@ -3,7 +3,7 @@ import Link from 'next/link'
 const Index = () => (
   <div>
   <Link href="/about">
-    <a style={{ fontSize: 20 }}>About Page</a>
+     <button>Go to About Page</button>
   </Link>
     <p>Hello Next.js</p>
   </div>
